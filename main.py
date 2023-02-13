c57ac5e5c182d8d8e24ce2e75d297de95ad344e4
juanjoibape <jotax2.dev@gmail.com>
2023-02-13 10:09:30 +0100

    Version 3.1 imagenes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 
 import sys
 
-pygame.init
+pygame.init()
 
 #definir colores
 NEGRO   = (  0,   0,   0)
@@ -21,14 +21,18 @@ pygame.display.set_caption("TheQuest")
 #pintar pantalla
 PANTALLA.fill(BLANCO)
 
+#fondo del juego
+fondo = pygame.image.load("imagenes/fondo_galaxia2.jpg")
+PANTALLA.blit(fondo,(0,0))
+
 #pintar un rectángulo
-pygame.draw.rect(PANTALLA, AZUL, (350, 400, 60, 40))
+pygame.draw.rect(PANTALLA, ROJO, (350, 400, 60, 40))
 #pintar cirulo
-pygame.draw.circle(PANTALLA, ROJO, (100, 100), 10, 0)
+pygame.draw.circle(PANTALLA, NEGRO, (100, 100), 10, 0)
 
 #mantener ventana abierta y poder cerrar, bucle principal
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    pygame.display.update()
+    pygame.display.update() #actualización de pantalla
